4819070cb6997bc01f30b0b0574b0260634b7329
Jonathan Heaney <jonathan.heaney@gmail.com>
2022-10-07 17:19:06 -0500

    Add comments, change paintings to constructor function

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
+// DOM variables
 const painting = document.querySelector('.painting');
 const info = document.querySelector('.info');
+let newPaintingBtn = document.querySelector('#new-painting');
+let pntg = document.querySelector('.painting');
+let title = document.querySelector('.title');
+let artist = document.querySelector('.artist');
+let year = document.querySelector('.year');
 
+// Constructor function to add new paintings
+function Painting(pntg, title, artist, year) {
+  this.pntg = pntg;
+  this.title = title;
+  this.artist = artist;
+  this.year = year;
+}
+
+// Add new paintings with constructor function
+const birthOfVenus = new Painting(
+  'img/birth-of-venus.jpeg',
+  'The Birth of Venus',
+  'Sandro Botticelli',
+  '1484-1486'
+);
+
+const starryNight = new Painting(
+  'img/starry-night.webp',
+  'Starry Night',
+  'Vincent Van Gogh',
+  '1889'
+);
+
+const theKiss = new Painting(
+  'img/the-kiss.jpeg',
+  'The Kiss',
+  'Gustav Klimt',
+  '1907-1908'
+);
+
+// Add all paintings created with the constructor function to the paintings array
+const paintings = [birthOfVenus, starryNight, theKiss];
+
+// Event listener when you click on the small version of the painting to check the answer.
+// Shows the full painting and the information/answer
 painting.addEventListener('click', () => {
   painting.classList.add('check');
   showAnswer();
@@ -9,86 +50,67 @@ painting.addEventListener('click', () => {
   painting.style.width = '100%';
 });
 
-let btn = document.querySelector('#new-painting');
-let pntg = document.querySelector('.painting');
-let title = document.querySelector('.title');
-let artist = document.querySelector('.artist');
-let year = document.querySelector('.year');
-
-const paintings = [
-  {
-    pntg: 'img/birth-of-venus.jpeg',
-    alt: 'The Birth of Venus',
-    title: 'The Birth of Venus',
-    artist: 'Sandro Botticelli',
-    year: '1484-1486',
-  },
-  {
-    pntg: 'img/starry-night.webp',
-    alt: 'Starry Night',
-    title: 'Starry Night',
-    artist: 'Vincent Van Gogh',
-    year: '1889',
-  },
-  {
-    pntg: 'img/the-kiss.jpeg',
-    alt: 'The Kiss',
-    title: 'The Kiss',
-    artist: 'Gustav Klimt',
-    year: '1907-1908',
-  },
-];
-
-btn.addEventListener('click', function () {
-  let random = Math.floor(Math.random() * paintings.length);
-  title.innerText = paintings[random].title;
-  artist.innerText = paintings[random].artist;
-  year.innerText = paintings[random].year;
-  pntg.style.backgroundImage = `url(${paintings[random].pntg})`;
-  pntg.classList.add('painting');
-  pntg.classList.remove('check');
-  pntg.alt = paintings[random].alt;
+// Event listener for the 'New Painting' button
+newPaintingBtn.addEventListener('click', function () {
+  setNewPainting();
   hideAnswer();
   setRandomPosition();
   setHeight();
   setWidth();
-  pntg.style.transition = 'none';
 });
 
+function setNewPainting() {
+  let random = Math.floor(Math.random() * paintings.length);
+  title.innerText = paintings[random].title;
+  artist.innerText = paintings[random].artist;
+  year.innerText = paintings[random].year;
+  pntg.alt = paintings[random].title;
+  pntg.style.backgroundImage = `url(${paintings[random].pntg})`;
+  pntg.classList.add('painting');
+  pntg.classList.remove('check');
+}
+
+// Hide/show the info section at the bottom
 function hideAnswer() {
   info.style.opacity = 0;
   info.style.pointerEvents = 'none';
 }
 
+function showAnswer() {
+  info.style.opacity = 1;
+  info.style.pointerEvents = 'auto';
+}
+
+// Show a random portion of the painting; will be completely different each time
 function setRandomPosition() {
   let randomXPosition = Math.floor(Math.random() * 100);
   let randomYPosition = Math.floor(Math.random() * 100);
   pntg.style.backgroundPosition = `${randomXPosition}%, ${randomYPosition}%`;
 }
 
+// Setting dimensions
 let height = 20;
 let width = 20;
 
+//Get the desired height from the user-input slider; default is 20%
 function getHeight(newHeight) {
   height = newHeight;
 }
 
+// Change the height to the user-inputted height
 function setHeight() {
   getHeight(height);
   pntg.style.height = `${height}%`;
 }
 
+// Get the desired width from the user-input slider; default is 20%
 function getWidth(newWidth) {
   width = newWidth;
 }
 
+// Change the width to the user-inputted width
 function setWidth() {
   getWidth(width);
   pntg.style.width = `${width}%`;
   pntg.style.margin = '0 auto';
-}
-
-function showAnswer() {
-  info.style.opacity = 1;
-  info.style.pointerEvents = 'auto';
 }
